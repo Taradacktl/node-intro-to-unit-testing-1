d66866c25a563dcf35db27f4e4cb7bfd0bb2104a
Taradacktl <35745484+Taradacktl@users.noreply.github.com>
2018-04-17 10:58:45 -0700

    Update test-fizz-buzzer.js

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,41 @@
+// import chai, declare expect variable
+const expect = require('chai').expect;
+
+// import fizzBuzzer
+const fizzBuzzer = require('../fizzBuzzer');
+
+// unit tests for our `fizzBuzzer` function
+describe('fizzBuzzer', function() {
+
+  // test the normal case
+  it('should return different words if divisible by 3, 5, or 15', function() {
+    // range of normal inputs, including
+    // notable cases like negative answers
+    const normalCases = [
+      {num: 15, expected: 'fizz-buzz'},
+      {num: 5, expected: 'buzz'},
+      {num: 3, expected: 'fizz'}
+    ];
+    // for each input (num), `fizzBuzzer` should
+    // produce the expected value
+    normalCases.forEach(function(input) {
+      const answer = adder(input.num);
+      expect(answer).to.equal(input.expected);
+    });
+  });
+
+  it('should raise error if args not numbers', function() {
+    // range of bad inputs where input is not a number
+    const badInputs = [
+      ['a'],
+      ['1'],
+      [false]
+    ];
+    // prove that an error is raised for bad inputs
+    badInputs.forEach(function(input) {
+      expect(function() {
+        fizBuzzer(input[0], input[1]);
+      }).to.throw(Error);
+    });
+  });
+});
